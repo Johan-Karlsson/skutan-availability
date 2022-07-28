666e4ac8144902e2321f136c04516b714749900e
Johan Karlsson <johan.karlsson@cevt.se>
2022-07-28 21:49:18 +0200

    initial commit with spring repo structure

---
+++ src/main/java/com/jk/skutan/SkutanApplication.java
@@ -0,0 +1,13 @@
+package com.jk.skutan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkutanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkutanApplication.class, args);
+	}
+
+}
